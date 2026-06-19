c29aa331a15c123380b3e12de0fda511629a556a
adityamore1705-cyber <adityamore1705@gmail.com>
2026-06-19 16:32:47 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -3,19 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load Dataset
 df = pd.read_csv("employee_data.csv")
 
-# Check Missing Values
 print(df.isnull().sum())
 
-# Fill Missing Values
 df.fillna(df.mean(numeric_only=True), inplace=True)
 
-# Remove Duplicates
 df.drop_duplicates(inplace=True)
 
-# Outlier Detection using IQR
 Q1 = df['Salary'].quantile(0.25)
 Q3 = df['Salary'].quantile(0.75)
 
@@ -27,7 +22,6 @@ upper = Q3 + 1.5 * IQR
 df = df[(df['Salary'] >= lower) &
         (df['Salary'] <= upper)]
 
-# Visualization
 sns.histplot(df['Salary'], kde=True)
 plt.title("Salary Distribution")
 plt.show()
@@ -39,4 +33,5 @@ plt.show()
 
 sns.heatmap(df.corr(numeric_only=True),
             annot=True)
+
 plt.show()
